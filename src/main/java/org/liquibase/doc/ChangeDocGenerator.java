3301377cc81af380b1fa8b1f950eaad9be581d4a
jajecnikmarci <jajecnikmarcell2004@gmail.com>
2024-11-11 21:37:22 +0100

    reformat code

--- src/main/java/org/liquibase/doc/ChangeDocGenerator.java
+++ src/main/java/org/liquibase/doc/ChangeDocGenerator.java
@@ -2,7 +2,7 @@ package org.liquibase.doc;
 
 import freemarker.template.*;
 import liquibase.change.*;
-import liquibase.change.core.*;
+import liquibase.change.core.CreateIndexChange;
 import liquibase.change.custom.CustomChangeWrapper;
 import liquibase.database.Database;
 import liquibase.database.core.MySQLDatabase;
@@ -15,80 +15,87 @@ import java.util.*;
 
 public class ChangeDocGenerator {
 
-    private static final Logger logger = LoggerFactory.getLogger(ChangeDocGenerator.class);
-
-    public static void main(String[] args) throws Exception {
-        logger.info("Generating xsd for all changes in included jar");
-        Map<String, SortedSet<Class<? extends Change>>> definedChanges = ChangeFactory.getInstance().getRegistry();
-        TreeMap<String, SortedSet<Class<? extends Change>>> sortedChanges = new TreeMap<>(definedChanges);
-        generateXSDwithFreeMarker(sortedChanges, new MySQLDatabase());
-    }
-
-
-    /**
-     * Wrapper class to store the default values along with the parameters
-     */
-    public static class ChangeParamMetaData extends ChangeParameterMetaData {
-        String defaultValue = "";
-        Class containedType = null;
-        final Change change;
-
-        ChangeParamMetaData(Database db, ChangeParameterMetaData orig, Change change) {
-            super(change, orig.getParameterName(),
-                    orig.getDisplayName(), orig.getDescription(),
-                    new HashMap<String, Object>() {{
-                        put(db.getShortName(), orig.getExampleValue(db));
-                    }}, orig.getSince(), orig.getDataTypeClass()
-                    , orig.getRequiredForDatabase().toArray(new String[0])
-                    , orig.getSupportedDatabases().toArray(new String[0])
-                    , orig.getMustEqualExisting(), orig.getSerializationType());
-            if (!orig.getDataType().contains(" of ")) {
-                try {
-                    Object o = orig.getCurrentValue(change);
-                    boolean isBool = "boolean".equals(orig.getDataType());
-                    if (!isBool || (null != o && (boolean) o)) {
-                        this.defaultValue = o == null ? "" : o.toString();
-                    }
-                } catch (Exception e) {
-                    logger.error("Error getting current value for: " + change.getSerializedObjectName()
-                            + " / " + orig.getParameterName(), e);
-                }
-            } else {
-                containedType = (Class) orig.getDataTypeClassParameters()[0];
-            }
-            this.change = change;
-        }
-
-        boolean isContainer() {
-            return null != containedType;
-        }
-
-        String getDefaultValue() {
-            return this.defaultValue;
-        }
-
-        public Class getContainedType() {
-            return containedType;
-        }
-
-        public boolean requiredForAll() {
-            return getRequiredForDatabase().contains(ALL);
-        }
-
-        boolean isNested() {return getSerializationType() == SerializationType.NESTED_OBJECT;}
-        boolean isDirectValue() {return getSerializationType() == SerializationType.DIRECT_VALUE;}
-    }
-
     /**
      * [change name] -> [columnConfig] map
      */
     static final Map<String, String> specialColumnConfig = new HashMap<>();
+    static final String ALL = "all";
+    /**
+     * List types should not be displayed
+     */
+    static final List<String> skipTypeForType = Arrays.asList("list", "databaseFunction", "sequenceNextValueFunction");
+    /**
+     * Display names for types
+     */
+    static final Map<String, String> typeDisplayName = new HashMap<>();
+    private static final Logger logger = LoggerFactory.getLogger(ChangeDocGenerator.class);
 
     static {
         specialColumnConfig.put("dropColumn", "DropColumnConfig");
         specialColumnConfig.put("createIndex", "IndexColumnConfig");
         specialColumnConfig.put("insert", "insertUpdateColumnConfig");
         specialColumnConfig.put("update", "insertUpdateColumnConfig");
+    }
+
+    static {
+        typeDisplayName.put("bigInteger", "integer");
+    }
+
+    public static void main(String[] args) throws Exception {
+        logger.info("Generating xsd for all changes in included jar");
+        TreeMap<String, SortedSet<Class<? extends Change>>> sortedChanges = new TreeMap<>(ChangeFactory.getInstance().getRegistry());
+        generateXSDwithFreeMarker(ChangeFactory.getInstance().getRegistry(), new MySQLDatabase());
+    }
+
+    /**
+     * Generate XSD file using FreeMarker template
+     *
+     * @param definedChanges         Changes to generate XSD for
+     * @param defaultExampleDatabase Default database to use for examples
+     * @throws IOException       if an I/O error occurs
+     * @throws TemplateException if an error occurs while processing the template
+     */
+    private static void generateXSDwithFreeMarker(Map<String, SortedSet<Class<? extends Change>>> definedChanges, MySQLDatabase defaultExampleDatabase) throws IOException, TemplateException {
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_31);
+        cfg.setClassForTemplateLoading(ChangeDocGenerator.class, "/templates");
+
+        cfg.setOutputEncoding("UTF-8");
+        cfg.setDefaultEncoding("UTF-8");
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+        cfg.setLogTemplateExceptions(false);
+        cfg.setWrapUncheckedExceptions(true);
+        cfg.setFallbackOnNullLoopVariable(false);
+        cfg.setObjectWrapper(new DefaultObjectWrapperBuilder(Configuration.VERSION_2_3_31).build());
+        cfg.setSharedVariable("esc", new freemarker.template.utility.XmlEscape());
+        Template xsdTemplate = cfg.getTemplate("changeDocTemplate.ftl");
+
+        Map<String, ChangeData> changeDataModel = new HashMap<>(); /// Map for the processed changes
+        for (String changeName : definedChanges.keySet()) {
+            Change change = ChangeFactory.getInstance().create(changeName);
+            ChangeData changeData = new ChangeData();
+            changeData.metaData = ChangeFactory.getInstance().getChangeMetaData(changeName);
+            List<ChangeParamMetaData> params = setExamples(defaultExampleDatabase, change, changeData.metaData);
+            params.forEach(param -> {
+                boolean shouldBePrinted = !skipTypeForType.contains(param.getDataType());
+                String dataType = null;
+                if (shouldBePrinted)
+                    dataType = convertDataTypeToXsdFormat(param);
+                ChangeData.ParamWithTypeFlag paramWithTypeFlag = new ChangeData.ParamWithTypeFlag(param, shouldBePrinted, dataType);
+                if (param.isNested()) { /// If the parameter is a container, add it to the nested parameters
+                    changeData.nestedParams.add(paramWithTypeFlag);
+                } else {
+                    changeData.params.add(paramWithTypeFlag);
+                }
+            });
+            changeDataModel.put(changeName, changeData);
+        }
+
+        Map<String, Object> xsdDataModel = new HashMap<>(); /// Map for the processed changes and for the template
+        xsdDataModel.put("changes", changeDataModel);
+
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("liquibase_changes.xsd"));
+        xsdTemplate.process(xsdDataModel, bufferedWriter);
+        bufferedWriter.close();
     }
 
     private static List<ChangeParamMetaData> setExamples(MySQLDatabase defaultExampleDatabase, Change exampleChange, ChangeMetaData changeMetaData) {
@@ -165,11 +172,98 @@ public class ChangeDocGenerator {
         return params;
     }
 
+    static String convertDataTypeToXsdFormat(ChangeParamMetaData param) {
+        switch (param.getDataType()) {
+            case "bigInteger":
+                return "integerExp";
+            case "boolean":
+                return "booleanExp";
+            case "string":
+                if (param.requiredForAll())
+                    return "nonEmptyString";
+                else
+                    return "xsd:string";
+            default:
+                return "xsd:" + param.getDataType();
+        }
+    }
+
+    /**
+     * Wrapper class to store the default values along with the parameters
+     */
+    public static class ChangeParamMetaData extends ChangeParameterMetaData {
+        final Change change;
+        String defaultValue = "";
+        Class containedType = null;
+
+        ChangeParamMetaData(Database db, ChangeParameterMetaData orig, Change change) {
+            super(change, orig.getParameterName(),
+                    orig.getDisplayName(), orig.getDescription(),
+                    new HashMap<String, Object>() {{
+                        put(db.getShortName(), orig.getExampleValue(db));
+                    }}, orig.getSince(), orig.getDataTypeClass()
+                    , orig.getRequiredForDatabase().toArray(new String[0])
+                    , orig.getSupportedDatabases().toArray(new String[0])
+                    , orig.getMustEqualExisting(), orig.getSerializationType());
+            if (!orig.getDataType().contains(" of ")) {
+                try {
+                    Object o = orig.getCurrentValue(change);
+                    boolean isBool = "boolean".equals(orig.getDataType());
+                    if (!isBool || (null != o && (boolean) o)) {
+                        this.defaultValue = o == null ? "" : o.toString();
+                    }
+                } catch (Exception e) {
+                    logger.error("Error getting current value for: " + change.getSerializedObjectName()
+                            + " / " + orig.getParameterName(), e);
+                }
+            } else {
+                containedType = (Class) orig.getDataTypeClassParameters()[0];
+            }
+            this.change = change;
+        }
+
+        boolean isContainer() {
+            return null != containedType;
+        }
+
+        String getDefaultValue() {
+            return this.defaultValue;
+        }
+
+        public Class getContainedType() {
+            return containedType;
+        }
+
+        public boolean requiredForAll() {
+            return getRequiredForDatabase().contains(ALL);
+        }
+
+        boolean isNested() {
+            return getSerializationType() == SerializationType.NESTED_OBJECT;
+        }
+
+        boolean isDirectValue() {
+            return getSerializationType() == SerializationType.DIRECT_VALUE;
+        }
+    }
 
     /**
      * Wrapper class to store the change metadata, parameters, and nested parameters for the FreeMarker template
      */
     public static class ChangeData {
+
+        /**
+         * Nested parameters of a change (children)
+         */
+        final List<ParamWithTypeFlag> nestedParams;
+        /**
+         * Parameters of a change, wrapped in ChangeParamMetaData
+         */
+        final List<ParamWithTypeFlag> params;
+        /**
+         * Metadata of a change
+         */
+        ChangeMetaData metaData;
 
         public ChangeData() {
             this.nestedParams = new ArrayList<>();
@@ -189,18 +283,16 @@ public class ChangeDocGenerator {
             return nestedParams;
         }
 
-        /**
-         * Nested parameters of a change (children)
-         */
-        final List<ParamWithTypeFlag> nestedParams;
-
-        /**
-         * Metadata of a change
-         */
-        ChangeMetaData metaData;
-
-        public static class ParamWithTypeFlag{
+        public static class ParamWithTypeFlag {
             final ChangeParamMetaData paramData;
+            final boolean shouldTypeBePrintedFlag;
+            final String dataType;
+
+            public ParamWithTypeFlag(ChangeParamMetaData paramData, boolean shouldTypeBePrintedFlag, String dataType) {
+                this.paramData = paramData;
+                this.shouldTypeBePrintedFlag = shouldTypeBePrintedFlag;
+                this.dataType = dataType;
+            }
 
             public boolean getShouldTypeBePrintedFlag() {
                 return shouldTypeBePrintedFlag;
@@ -209,108 +301,16 @@ public class ChangeDocGenerator {
             public ChangeParamMetaData getParamData() {
                 return paramData;
             }
+
             public int isRequiredForAll() {
                 if (paramData.requiredForAll()) return 1;
                 else return 0;
             }
 
-            final boolean shouldTypeBePrintedFlag;
-
             public String getDataType() {
                 return dataType;
             }
-
-            final String dataType;
-            public ParamWithTypeFlag(ChangeParamMetaData paramData, boolean shouldTypeBePrintedFlag, String dataType) {
-                this.paramData = paramData;
-                this.shouldTypeBePrintedFlag = shouldTypeBePrintedFlag;
-                this.dataType = dataType;
-            }
         }
-        /**
-         * Parameters of a change, wrapped in ChangeParamMetaData
-         */
-        final List<ParamWithTypeFlag> params;
-    }
-
-    /**
-     * Generate XSD file using FreeMarker template
-     * @param definedChanges Changes to generate XSD for
-     * @param defaultExampleDatabase Default database to use for examples
-     * @throws IOException if an I/O error occurs
-     * @throws TemplateException if an error occurs while processing the template
-     */
-    private static void generateXSDwithFreeMarker(Map<String, SortedSet<Class<? extends Change>>> definedChanges, MySQLDatabase defaultExampleDatabase) throws IOException, TemplateException {
-        Configuration cfg = new Configuration(Configuration.VERSION_2_3_31);
-        cfg.setClassForTemplateLoading(ChangeDocGenerator.class, "/templates");
-
-        cfg.setOutputEncoding("UTF-8");
-        cfg.setDefaultEncoding("UTF-8");
-        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
-        cfg.setLogTemplateExceptions(false);
-        cfg.setWrapUncheckedExceptions(true);
-        cfg.setFallbackOnNullLoopVariable(false);
-        cfg.setObjectWrapper(new DefaultObjectWrapperBuilder(Configuration.VERSION_2_3_31).build());
-        cfg.setSharedVariable("esc", new freemarker.template.utility.XmlEscape());
-        Template xsdTemplate = cfg.getTemplate("changeDocTemplate.ftl");
-
-        Map<String, ChangeData> changeDataModel = new HashMap<>(); /// Map for the processed changes
-        for (String changeName : definedChanges.keySet()) {
-            Change change = ChangeFactory.getInstance().create(changeName);
-            ChangeData changeData = new ChangeData();
-            changeData.metaData = ChangeFactory.getInstance().getChangeMetaData(changeName);
-            List<ChangeParamMetaData> params=  setExamples(defaultExampleDatabase, change, changeData.metaData);
-            params.forEach(param -> {
-                boolean shouldBePrinted = ! skipTypeForType.contains(param.getDataType());
-                String dataType = null;
-                if (shouldBePrinted)
-                    dataType = convertDataTypeToXsdFormat(param);
-                ChangeData.ParamWithTypeFlag paramWithTypeFlag = new ChangeData.ParamWithTypeFlag(param,shouldBePrinted, dataType);
-                if (param.isNested()) { /// If the parameter is a container, add it to the nested parameters
-                    changeData.nestedParams.add(paramWithTypeFlag);
-                }
-                else {
-                    changeData.params.add(paramWithTypeFlag);
-                }
-            });
-            changeDataModel.put(changeName, changeData);
-        }
-
-        Map<String, Object> xsdDataModel = new HashMap<>(); /// Map for the processed changes and for the template
-        xsdDataModel.put("changes", changeDataModel);
-
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("liquibase_changes.xsd"));
-        xsdTemplate.process(xsdDataModel, bufferedWriter);
-        bufferedWriter.close();
-    }
-
-    static String convertDataTypeToXsdFormat(ChangeParamMetaData param) {
-        switch (param.getDataType()) {
-            case "bigInteger":
-                return "integerExp";
-            case "string":
-                if (param.requiredForAll())
-                    return "nonEmptyString";
-                else
-                    return "xsd:string";
-            default:
-                return"xsd:" + param.getDataType();
-        }
-    }
-
-    final static String ALL = "all";
-
-    /**
-     * List types should not be displayed
-     */
-    final static List<String> skipTypeForType = Arrays.asList( "list", "databaseFunction", "sequenceNextValueFunction");
-    /**
-     * Display names for types
-     */
-    final static Map<String, String> typeDisplayName = new HashMap<>();
-
-    static {
-        typeDisplayName.put("bigInteger", "integer");
     }
 
 }
